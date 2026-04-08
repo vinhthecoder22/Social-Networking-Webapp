be5d2b80f7106bcbc0a617d878c1604bc21621f5
vinhthecoder22 <nthevinh225@gmail.com>
2026-04-08 19:33:14 +0700

    init: base spring boot project structure

---
+++ src/main/java/com/example/socialnetworkingbackend/SocialNetworkingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialnetworkingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socialnetworkingbackend/SocialNetworkingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialnetworkingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
