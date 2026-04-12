72bf1c54427e503780f69e70220a97bdcc541119
vinhthecoder22 <nthevinh225@gmail.com>
2026-04-12 10:38:54 +0700

    feat(config): add async executor configuration

---
+++ src/main/java/com/example/socialnetworkingbackend/config/AsyncConfig.java
@@ -0,0 +1,57 @@
+package com.example.socialnetworkingbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AsyncThread-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean(name = "rawVideoExecutor")
+    public ThreadPoolTaskExecutor rawVideoExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("VideoExecutor-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean(name = "rawAudioExecutor")
+    public ThreadPoolTaskExecutor rawAudioExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("AudioExecutor-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean(name = "rawImageExecutor")
+    public ThreadPoolTaskExecutor rawImageExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("ImageExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
